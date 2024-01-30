052274cd40e9d16fdfb706ca0e3ad62f849ac474
elruud <elisabeth.rusas.ruud@entur.org>
2024-01-30 16:30:45 +0100

    springboot setup

--- src/main/java/no/ignite/githubActionsCourse/GithubActionsCourseApplication.java
+++ src/main/java/no/ignite/githubActionsCourse/GithubActionsCourseApplication.java
@@ -8,6 +8,8 @@ public class GithubActionsCourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionsCourseApplication.class, args);
+		String text = "HÆLLÆ WORLD";
+		System.out.println(text);
 	}
 
 }
