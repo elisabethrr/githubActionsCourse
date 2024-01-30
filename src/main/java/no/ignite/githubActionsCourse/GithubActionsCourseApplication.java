3cb5e85f48230f1d42680f7ee2f992f3baf582e9
elruud <elisabeth.rusas.ruud@entur.org>
2024-01-30 16:10:39 +0100

    new spring project

---
+++ src/main/java/no/ignite/githubActionsCourse/GithubActionsCourseApplication.java
@@ -0,0 +1,13 @@
+package no.ignite.githubActionsCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/ignite/githubActionsCourse/GithubActionsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ignite.githubActionsCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
